272f334d2d4dfa396bcb6883b3448e55d296aea4
Mikołaj <malolepszy.mikolaj00@gmail.com>
2026-05-27 21:37:35 +0200

    rm notebooks2

--- src/paths.py
+++ src/paths.py
@@ -5,7 +5,6 @@ class Paths:
     PROJECT_ROOT = Path(__file__).resolve().parent.parent
     DATA_DIR = PROJECT_ROOT / "data"
     MODEL_DIR = PROJECT_ROOT / "models"
-    NOTEBOOK_DIR = PROJECT_ROOT / "notebooks"
 
     MNIST_MODEL = MODEL_DIR / "resnet18_mnist.pth"
     CIFAR10_MODEL = MODEL_DIR / "resnet18_cifar10.pth"
